1a06b189e67c45dc2f915aec8613e56d640e6735
Hudson Holanda <hudsonholanda55@gmail.com>
2024-05-21 19:45:49 -0300

    feat: add code

---
+++ src/server.js
@@ -0,0 +1,27 @@
+import http from 'http';
+
+const users = [];
+
+const server = http.createServer((req, res) => {
+  const { method, url } = req;
+
+  if(method === 'GET' && url === '/users') {
+    return res
+      .setHeader('Content-type', 'application/json').end(JSON.stringify(users));
+  }
+
+  if(method === 'POST' && url === '/users') {
+    users.push({
+      id: 1,
+      name: 'Jhon Doe',
+      email: 'jhondoe@example.com'
+    })
+
+    return res.end('Criação de usuários');
+  }
+
+
+  return res.end('Hello test')
+});
+
+server.listen(3333);
