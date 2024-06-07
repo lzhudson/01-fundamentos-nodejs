587af5d3e723db94abf525b1139a00022fe8cef2
Hudson Holanda <hudsonholanda55@gmail.com>
2024-06-07 08:38:30 -0300

    feat: persit database

--- src/database.js
+++ src/database.js
@@ -1,5 +1,26 @@
+import fs from 'node:fs/promises';
+
+const databasePath = new URL('../db.json', import.meta.url);
+
+
+
 export class Database {
   #database = {}
+
+
+  constructor() {
+    fs.readFile(databasePath, 'utf-8')
+      .then(data => {
+        this.#database = JSON.parse(data)
+      })
+      .catch(() => {
+        this.#persist()
+      })
+  }
+
+  #persist() {
+    fs.writeFile(databasePath, JSON.stringify(this.#database))
+  }
 
   select(table) {
     const data = this.#database[table] ?? []
@@ -13,5 +34,9 @@ export class Database {
     } else {
       this.#database[table] = [data]
     }
+
+    this.#persist();
+
+    return data
   }
 }
